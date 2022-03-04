50ca7e35ec03f61b7178bc71fb738c3dc3889b3e
Brian Davis <fosten@gmail.com>
2022-03-04 14:20:14 -0500

    Move customBlocks import from config.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import customBlocks from '@Fosten/volto-itxbb-theme/components/Blocks/customBlocks'
+
 const applyConfig = (config) => {
+  config.blocks = {
+    ...config.blocks,
+    blocksConfig: {
+      ...config.blocks.blocksConfig,
+      ...customBlocks},
+  }
   return config;
-};
+}
 
 export default applyConfig;
